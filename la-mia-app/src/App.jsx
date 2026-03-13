2b95ff66e2626a15a422211cb357f1ed7267ea9b
SamuPr. <provaroni.samuele@itisgrassi.edu.it>
2026-03-13 13:53:34 +0000

    111

---
+++ la-mia-app/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+// Questo è un Componente React
+function Benvenuto() {
+  return (
+    <div className="card">
+      <h1>Ciao a tutti!</h1>
+      <p>Questo è il mio primo componente.</p>
+    </div>
+  );
+}
+
+export default Benvenuto;
